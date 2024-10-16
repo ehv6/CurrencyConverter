951f706a2283722ce53739caa268d0aadaf4d724
Tushig Battulga <ehv6@txstate.edu>
2024-10-15 22:51:09 -0500

    Initial variables and prompts added

--- src/main/java/com/CurrencyConverter.java
+++ src/main/java/com/CurrencyConverter.java
@@ -1,6 +1,19 @@
 package com;
 
+import java.util.Scanner;
+
 public class CurrencyConverter {
 
+    puclic static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Converting From: ");
 
+        String convertFrom = scanner.nextLine();
+        System.out.print("Converting To: ");
+
+        System.out.println("Quantity: ");
+        BigDecimal quantity = scanner.nextBigDecimal();
+
+        
+    }
 }
